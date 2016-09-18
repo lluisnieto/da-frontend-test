6bb8ca48f78d52d7a7db9a5d5ca344cdf19517db
Lluis <anconetano@gmail.com>
2016-09-18 21:49:17 +0200

    Fix: bug clicking label (bubbling event)

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,7 +3,10 @@ firstClassTable();
 
 
 function clearValues () {
-
+    var _elems =  document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < _elems.length; i++) {
+        _elems[i].checked = false;
+    }
 }
 
 function firstClassTable () {
@@ -14,33 +17,22 @@ function firstClassTable () {
             // console.log(e);
             var _this = e.target;
             var _row =  _this.closest('tr');
-            var _checkbox = _row.querySelectorAll('input[type="checkbox"');
+            var _checkbox = _row.querySelectorAll('input[type="checkbox"]');
             var _propState = _checkbox[0].checked;
-console.log(_row);
+
+            if(_this.tagName == 'LABEL') { return false }
+
             toggleClass('active', _row);
 
-            if (!(
-                e.target.tagName == 'INPUT' ||
-                e.target.tagName == 'LABEL'
-                )){
+            if (e.target.tagName != 'INPUT'){
                 _propState = toggleState(_propState);
                 _checkbox[0].checked = _propState;
             }
         });
     }
-
-        /*var _row = $(this).closest('tr');
-        var _checkbox  = $(this).closest('tr').first('td').find('input[type="checkbox"]');
-        var _propState = $(_checkbox).prop('checked');
-
-        $(_row).toggleClass('active');
-
-        if (! ($(event.target).is('input') || $(event.target).is('label'))) {
-            _propState === true ? _propState = false : _propState = true;
-            $(_checkbox).prop('checked', _propState);
-        }
-    });*/
 }
+
+
 
 function toggleState (state) {
     state === true ? state = false : state = true;
